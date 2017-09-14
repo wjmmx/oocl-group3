cf293e7ca496b9813bc968a53d0a2af5c09f288e
Junno Beringuela <junno.paolo.beringuela@oocl.com>
2017-09-14 20:23:10 +0800

    dummy storage

--- Group3.Tennisee/src/main/java/com/group3/tennisee/Storage.java
+++ Group3.Tennisee/src/main/java/com/group3/tennisee/Storage.java
@@ -8,6 +8,18 @@ public class Storage {
 	
 	public Storage() {
 		this.scheduleList = new ArrayList<Schedule>();
+		scheduleList.add(new Schedule("A1", "A", "Monday", "09:00", "11:00"));
+		scheduleList.add(new Schedule("A2", "A", "Monday", "11:00", "13:00"));
+		scheduleList.add(new Schedule("A3", "A", "Monday", "13:00", "15:00"));
+		scheduleList.add(new Schedule("A4", "A", "Monday", "15:00", "17:00"));
+		scheduleList.add(new Schedule("A5", "A", "Monday", "17:00", "19:00"));
+		scheduleList.add(new Schedule("A6", "A", "Monday", "19:00", "21:00"));
+		scheduleList.add(new Schedule("A1", "B", "Monday", "09:00", "11:00"));
+		scheduleList.add(new Schedule("A2", "B", "Monday", "11:00", "13:00"));
+		scheduleList.add(new Schedule("A3", "B", "Monday", "13:00", "15:00"));
+		scheduleList.add(new Schedule("A4", "B", "Monday", "15:00", "17:00"));
+		scheduleList.add(new Schedule("A5", "B", "Monday", "17:00", "19:00"));
+		scheduleList.add(new Schedule("A6", "B", "Monday", "19:00", "21:00"));
 	}
 	
 	public ArrayList<Schedule> getScheduleList() {
